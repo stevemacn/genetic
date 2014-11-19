822e4739cbcf566efc80e3c6de02e666bc8e89ed
Stephen MacNeil <smacneil01@gmail.com>
2014-11-18 22:31:38 -0500

    project configuration file

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject hello-world "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.2.0"]
+                 [ring/ring-defaults "0.1.2"]]
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler hello-world.core.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
