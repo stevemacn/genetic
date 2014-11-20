28aa9a4fdf33c3583ce5208981ace7a055997f35
Stephen MacNeil <smacneil01@gmail.com>
2014-11-20 17:31:21 -0500

    render html page from route via hiccup

--- src/myapp/core.clj
+++ src/myapp/core.clj
@@ -1,12 +1,17 @@
 (ns myapp.core
   (:require [compojure.core :refer :all]
-
+            [hiccup.core :as hiccup]
             [org.httpkit.server :refer [run-server]])) ; httpkit is a server
 
-
+(defn d3-page []
+  (hiccup/html
+    [:head
+     [:title "Equation fitter"]]
+    [:body
+     [:div {:id "goalGraph"}]]))
 
 (defroutes myapp
-  (GET "/" [] (fn [req] "hello"))
+  (GET "/" [] (fn [req] (d3-page)))
   (GET "/hello" [] "Hello World"))
 
 
