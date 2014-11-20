b2eafb0c0c626715fdf785f93b11232bfcdfe142
Stephen MacNeil <smacneil01@gmail.com>
2014-11-20 18:45:02 -0500

    added static files for rendered page

--- src/myapp/core.clj
+++ src/myapp/core.clj
@@ -1,18 +1,22 @@
 (ns myapp.core
   (:require [compojure.core :refer :all]
+            [compojure.route :as cpj.route]
             [hiccup.core :as hiccup]
             [org.httpkit.server :refer [run-server]])) ; httpkit is a server
 
 (defn d3-page []
   (hiccup/html
     [:head
-     [:title "Equation fitter"]]
+      [:title "Equation fitter"]]
     [:body
-     [:div {:id "goalGraph"}]]))
+      [:div {:id "goalGraph"}]
+      [:script {:src "http://d3js.org/d3.v3.min.js"}]
+      [:script {:src "/linegraph.js" }]]))
 
 (defroutes myapp
   (GET "/" [] (fn [req] (d3-page)))
   (GET "/hello" [] "Hello World"))
+
 
 
 (defn -main [& args]
