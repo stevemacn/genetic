9d47d101e5cd9f1cbd1dc7dbf111d9f1a2b67720
Stephen MacNeil <smacneil01@gmail.com>
2014-11-20 17:16:53 -0500

    update the configuration requirements

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.2.0"]
-                 [ring/ring-defaults "0.1.2"]]
+                 [hiccup "1.0.5"]
+                 [http-kit "2.1.16"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler hello-world.core.handler/app}
   :profiles
