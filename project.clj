76dc9a97d2a6089951250eb3b52494d31f4ed5c6
Stephen MacNeil <smacneil01@gmail.com>
2014-11-22 11:20:55 -0500

    add dependency to run server in REPL

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.2.0"]
                  [hiccup "1.0.5"]
+                 [ring/ring-jetty-adapter "1.3.1"]
                  [http-kit "2.1.16"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler hello-world.core.handler/app}
